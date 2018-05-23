fef4b2f9a894d30a39c914ff00bfa669d5bebe48
denysivanov <denys.ivanov@gmail.com>
2018-05-23 14:39:00 -0400

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Masha Ivanova My Heart 123
+        Masha Ivanova My Heart 123 Samaya Luchshaya
       </div>
       <div class="col-lg-6">
       </div>
