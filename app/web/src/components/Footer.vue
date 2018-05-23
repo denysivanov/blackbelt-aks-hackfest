1a6e6802acf6d1c2774b0a48d0207471dfd95b58
denysivanov <denys.ivanov@gmail.com>
2018-05-23 14:27:43 -0400

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Masha Ivanova
+        Masha Ivanova My Heart
       </div>
       <div class="col-lg-6">
       </div>
